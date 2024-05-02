4040d39f28e449e003052b89bffffd8fe1d320a3
Kevin De Nichilo <kevin.palomino2002@gmail.com>
2024-05-02 13:56:53 -0300

    entrega

--- src/app.js
+++ src/app.js
@@ -1,28 +1,44 @@
 const express = require("express");
 const app = express();
-const exphbs = require("express-handlebars");
 const PUERTO = 8080;
-require("./database.js");
+const usuariosRouter = require("./routes/usuarios.router.js");
+const manejadorError = require("./middleware/error.js");
 
-const productsRouter = require("./routes/products.router.js");
-const cartsRouter = require("./routes/carts.router.js");
-const viewsRouter = require("./routes/views.router.js");
+//Utilizamos compresión:  npm install express-compression
+const compression = require("express-compression");
 
 //Middleware
-app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("./src/public"));
+app.use(express.urlencoded({ extended: true }));
+//app.use(compression());
 
-//Handlebars
-app.engine("handlebars", exphbs.engine());
-app.set("view engine", "handlebars");
-app.set("views", "./src/views");
+// app.use(compression({
+//     brotli: {
+//         enabled: true,
+//         zlib: {}
+//     }
+// }));
 
-//Rutas:
-app.use("/api/products", productsRouter);
-app.use("/api/carts", cartsRouter);
-app.use("/", viewsRouter);
+//Middleware para el manejo de errores:
+
+app.use("/usuarios", usuariosRouter);
+app.use(manejadorError);
+
+//Rutas
+// app.get("/", (req, res) => {
+//     let string = "Hola coders, somos programadores y no sabemos arreglar impresoras";
+
+//     for (let i = 0; i < 5e4; i++) {
+//         string += "Hola coders, somos programadores y no sabemos arreglar impresoras";
+//     }
+
+//     res.send(string);
+// })
 
 app.listen(PUERTO, () => {
-  console.log(`Servidor escuchando en el puerto ${PUERTO}`);
+  console.log("Escuchando en el puerto de Mar del Plata");
 });
+
+//Sin compresión: 1.3 mb
+//Con compresión 11.4 kb
+//Con brotli 357 bytes
