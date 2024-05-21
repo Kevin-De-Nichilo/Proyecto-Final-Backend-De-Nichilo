f61d2ff05e438c765d61a2578a61df294c63ee59
Kevin De Nichilo <kevin.palomino2002@gmail.com>
2024-05-21 19:04:11 -0300

    tercera practica integradora

--- src/app.js
+++ src/app.js
@@ -1,44 +1,77 @@
 const express = require("express");
 const app = express();
+const exphbs = require("express-handlebars");
+const cookieParser = require("cookie-parser");
+const passport = require("passport");
+const initializePassport = require("./config/passport.config.js");
+const cors = require("cors");
+const path = require("path");
 const PUERTO = 8080;
-const usuariosRouter = require("./routes/usuarios.router.js");
-const manejadorError = require("./middleware/error.js");
+require("./database.js");
 
-//Utilizamos compresión:  npm install express-compression
-const compression = require("express-compression");
+const productsRouter = require("./routes/products.router.js");
+const cartsRouter = require("./routes/carts.router.js");
+const viewsRouter = require("./routes/views.router.js");
+const userRouter = require("./routes/user.router.js");
 
 //Middleware
-app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-//app.use(compression());
+app.use(express.json());
+//app.use(express.static("./src/public"));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(cors());
 
-// app.use(compression({
-//     brotli: {
-//         enabled: true,
-//         zlib: {}
-//     }
-// }));
+//Passport
+app.use(passport.initialize());
+initializePassport();
+app.use(cookieParser());
 
-//Middleware para el manejo de errores:
+//AuthMiddleware
+const authMiddleware = require("./middleware/authmiddleware.js");
+app.use(authMiddleware);
 
-app.use("/usuarios", usuariosRouter);
-app.use(manejadorError);
+//Handlebars
+app.engine("handlebars", exphbs.engine());
+app.set("view engine", "handlebars");
+app.set("views", "./src/views");
 
-//Rutas
-// app.get("/", (req, res) => {
-//     let string = "Hola coders, somos programadores y no sabemos arreglar impresoras";
+//Rutas:
+app.use("/api/products", productsRouter);
+app.use("/api/carts", cartsRouter);
+app.use("/api/users", userRouter);
+app.use("/", viewsRouter);
 
-//     for (let i = 0; i < 5e4; i++) {
-//         string += "Hola coders, somos programadores y no sabemos arreglar impresoras";
-//     }
-
-//     res.send(string);
-// })
-
-app.listen(PUERTO, () => {
-  console.log("Escuchando en el puerto de Mar del Plata");
+const httpServer = app.listen(PUERTO, () => {
+  console.log(`Servidor escuchando en el puerto ${PUERTO}`);
 });
 
-//Sin compresión: 1.3 mb
-//Con compresión 11.4 kb
-//Con brotli 357 bytes
+///Websockets:
+const SocketManager = require("./sockets/socketmanager.js");
+new SocketManager(httpServer);
+
+//Codigo actualizar cantidades:
+// async updateProductQuantity(cartId, productId, updatedQuantity) {
+//     try {
+//       const cartItem = await CartModel.findById(cartId);
+//       if (!cartItem) {
+//         throw new Error('Invalid cart id');
+//       }
+
+//       const product = cartItem.products.find(
+//         (item) => item.product._id.toString() === productId
+//       );
+
+//       if (!product) {
+//         throw new Error('Invalid product id');
+//       }
+
+//       product.quantity = updatedQuantity;
+//       cartItem.markModified('products');
+//       await cartItem.save();
+
+//       return cartItem;
+//     } catch (error) {
+//       console.error('Error while updating the product quantity', error);
+//       throw new Error(error);
+//     }
+//   }
