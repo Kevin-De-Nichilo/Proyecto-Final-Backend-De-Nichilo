5f66c2148308ce0064c92885cc9745c0b3bbdbf7
Kevin De Nichilo <kevin.palomino2002@gmail.com>
2024-07-26 17:35:58 -0300

    final

--- adoptame-front/src/api/config.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const axiosInstance = axios.create({
-  baseURL: "https://50045adoptame-qa.up.railway.app/",
-  withCredentials: true,
-});
-
-export default axiosInstance;
